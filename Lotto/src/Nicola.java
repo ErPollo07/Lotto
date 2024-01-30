a75bdb3bff2fb6330e4b7b175de7d9472e9f500e
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2024-01-30 18:15:01 +0100

    Done the function for calculation the winning prize

--- Lotto/src/Nicola.java
+++ Lotto/src/Nicola.java
@@ -2,4 +2,25 @@ public class Nicola {
     public static void main(String[] args) {
 
     }
+
+    public static double winningPrize(int amount, int ruote) {
+        double[][] prizes = {
+                {11.23},
+                {5.62, 250.00},
+                {3.74, 83.33, 4500.00},
+                {2.81, 41.67, 1125.00, 120000.00},
+                {2.25, 25.00, 450.00,  24000.00, 6000000.00},
+                {1.87, 16.67, 225.00,  8000.00,  1000000.00},
+                {1.60, 11.90, 128.57,  3428.57,  285714.29},
+                {1.40, 8.93,  80.36,   1714.29,  107142.86},
+                {1.25, 6.94,  53.57,   952.38,   47619.05},
+                {1.12, 5.56,  37.50,   571.43,   23809.52}
+        };
+
+        double winning;
+
+        winning = prizes[4][4] * amount / ruote;
+
+        return winning;
+    }
 }
