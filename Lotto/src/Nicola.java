9516d59104bc893b9cfc2d1967c2105333cdc3f1
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2024-01-31 14:49:38 +0100

    Added methods: playerNumbers, extractedWheel, valueChecker, valoreRandom, retriveAmount, factorial

--- Lotto/src/Nicola.java
+++ Lotto/src/Nicola.java
@@ -1,9 +1,100 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Nicola {
     public static void main(String[] args) {
-
+        int[] playerNumbers;
+        int[] wheel;
+        
+        wheel = extractedWheel();
+        
+        playerNumbers = playerNumbers();
     }
 
-    public static double winningPrize(int amount, int ruote, int playedNumbers, int colum) {
+
+    private static double calculationWinningPrice() {
+
+
+        return 0;
+    }
+
+    private static int[] playerNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+        boolean[] n = new boolean[90];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("inserisci un numero: ");
+            numbers[i] = scanner.nextInt();
+
+            // if in the array of boolean there is a true it's indicate that there is already a number
+            while (n[numbers[i] - 1]) {
+                // Ask to reinsert the number
+                System.out.println("Numero gia inserito. Iserisci un altro numero: ");
+                numbers[i] = scanner.nextInt();
+            }
+
+            // Set the correct pos in the array to true to indicate that there is the number
+            n[numbers[i] - 1] = true;
+        }
+
+        return numbers;
+    }
+
+    public static int[] extractedWheel() {
+        int[] numeri = new int[5];
+        int minValue = 1, maxValue = 90;
+
+        for (int i = 0; i < numeri.length; i++) {
+            numeri[i] = valoreRandom(minValue, maxValue); // Assign random value to numeri[i]
+
+            if (i > 0) valueChecker(numeri, i, minValue, maxValue); // check if the value isn't repeating
+        }
+
+        return numeri; // return array
+    }
+
+    // Check if the value is available
+    private static void valueChecker(int[] array, int i, int minValue, int maxValue) {
+        for (int k = 0; k < i; k++) {
+            // If the value is already in the array, reassign it and recheck all array
+            if (array[i] == array[k]) {
+                array[i] = valoreRandom(minValue, maxValue); // Assign another random value to array[i]
+                k = -1; // Restart the check
+            }
+        }
+    }
+
+    private static int valoreRandom(int minValue, int maxValue)
+    {
+        Random casuale = new Random();
+
+        return  casuale.nextInt(minValue,maxValue+1);
+    }
+
+    private static int[] retrivePlayerBetypes() {
+        Scanner scanner = new Scanner(System.in);
+        int[] betTypes = new int[5];
+        int userBet = 1;
+
+
+        for (int i = 0; i < betTypes.length && userBet != 0; i++) {
+            do {
+                System.out.println("Inserisci il " + (i+1) + " numero: ");
+                userBet = scanner.nextInt();
+
+                if (userBet < 0 || userBet > 5) System.out.println("Numero non valido");
+            } while (userBet < 0 || userBet > 5);
+
+            // If the userBet is different from 0
+            if (userBet != 0)
+                betTypes[userBet - 1] = userBet; // Insert in the correct place the number
+        }
+
+        return betTypes;
+    }
+
+    public static double winningPrize(int amount, int wheels, int playedNumbers, int betType) {
         double[][] prizes = {
                 {11.23},
                 {5.62, 250.00},
@@ -19,8 +110,24 @@ public class Nicola {
 
         double winning;
 
-        winning = prizes[playedNumbers][colum] * amount / ruote;
+        winning = prizes[playedNumbers][betType] * amount / wheels;
 
         return winning;
+    }
+
+    /*
+    userWins = player numbers equals to the wheel
+    betType = sing, ambo, terna, quaterna, cinquina
+    */
+    private static int retriveWinAmount(int betType, int userWins) {
+        return (factorial(userWins)) / (factorial(betType) * factorial(userWins - betType));
+    }
+
+    /* Give the factorial of a number */
+    private static int factorial(int input) {
+        for (int inputMultiplier = input; inputMultiplier > 0; inputMultiplier--)
+            input *= inputMultiplier;
+
+        return input;
     }
 }
